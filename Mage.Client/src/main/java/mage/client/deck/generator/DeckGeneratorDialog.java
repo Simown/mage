29868cc48755cf930fedffe677454e7ee76e205a
Simown <simownuk@gmail.com>
2015-06-26 11:57:18 +0100

    Tweak singleton to specificy only allow non-land to be singleton

--- Mage.Client/src/main/java/mage/client/deck/generator/DeckGeneratorDialog.java
+++ Mage.Client/src/main/java/mage/client/deck/generator/DeckGeneratorDialog.java
@@ -116,7 +116,7 @@ public class DeckGeneratorDialog {
 
         // Singletons
         cSingleton = new JCheckBox("Singleton", false);
-        cSingleton.setToolTipText("Allow only a single copy of each card in your deck.");
+        cSingleton.setToolTipText("Allow only a single copy of each non-land card in your deck.");
         String singletonEnabled = PreferencesDialog.getCachedValue(PreferencesDialog.KEY_NEW_DECK_GENERATOR_SINGLETON, "false");
         cSingleton.setSelected(Boolean.valueOf(singletonEnabled));
         jCheckBoxes.add(cSingleton);
